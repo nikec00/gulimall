42d9a3e6c41a89e2bc8aa331637d9c9b093eaccf
nikec00 <760144709@qq.com>
2021-11-07 14:08:47 +0800

    商品系统菜单三级分类查询

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GuLiMallCorsConfiguration.java
@@ -0,0 +1,32 @@
+package com.atguigu.gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @Description： 跨域配置类
+ * @Author: nkc
+ * @Date: 2021/11/7 13:18
+ */
+@Configuration
+public class GuLiMallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        //1、配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
