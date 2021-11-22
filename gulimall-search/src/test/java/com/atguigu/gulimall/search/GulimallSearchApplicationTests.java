c4656ed2fa30b249022c8c57c0e909eb0e40ec87
nikec00 <760144709@qq.com>
2021-11-22 21:44:43 +0800

    搜索服务

--- gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,13 +1,84 @@
 package com.atguigu.gulimall.search;
 
-import org.junit.jupiter.api.Test;
+import com.alibaba.fastjson.JSON;
+import com.atguigu.gulimall.search.config.GulimallElasticSearchConfig;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @SpringBootTest
-class GulimallSearchApplicationTests {
+@RunWith(SpringRunner.class)
+public class GulimallSearchApplicationTests {
+
+    @Autowired
+    private RestHighLevelClient client;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.out.println(client);
+    }
+
+    class User {
+        private String userName;
+
+        private String gender;
+
+        private Integer age;
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+    }
+
+    /**
+     * 存储数据到es
+     */
+    @Test
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");
+//        indexRequest.source("userName","张三","age",18,"gender","男");
+        User user = new User();
+        user.setUserName("张三");
+        user.setAge(22);
+        user.setGender("男");
+        String jsonString = JSON.toJSONString(user);
+        indexRequest.source(jsonString, XContentType.JSON);
+        // 执行操作
+        IndexResponse index = client.index(indexRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+
+        // 提取有用的响应数据
+        System.out.println(index);
+
     }
 
 }
