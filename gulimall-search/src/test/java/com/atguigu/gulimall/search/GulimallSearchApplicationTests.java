9ba2a859711b898a5c825bd2bea1f31ae224159d
nikec00 <760144709@qq.com>
2021-11-23 14:06:10 +0800

    搜索服务

--- gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -4,13 +4,23 @@ import com.alibaba.fastjson.JSON;
 import com.atguigu.gulimall.search.config.GulimallElasticSearchConfig;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.Avg;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -25,6 +35,126 @@ public class GulimallSearchApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println(client);
+    }
+
+    static class Account {
+
+        private int account_number;
+        private int balance;
+        private String firstname;
+        private String lastname;
+        private int age;
+        private String gender;
+        private String address;
+        private String employer;
+        private String email;
+        private String city;
+        private String state;
+
+        public void setAccount_number(int account_number) {
+            this.account_number = account_number;
+        }
+
+        public int getAccount_number() {
+            return account_number;
+        }
+
+        public void setBalance(int balance) {
+            this.balance = balance;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setEmployer(String employer) {
+            this.employer = employer;
+        }
+
+        public String getEmployer() {
+            return employer;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        @Override
+        public String toString() {
+            return "Account{" +
+                    "account_number=" + account_number +
+                    ", balance=" + balance +
+                    ", firstname='" + firstname + '\'' +
+                    ", lastname='" + lastname + '\'' +
+                    ", age=" + age +
+                    ", gender='" + gender + '\'' +
+                    ", address='" + address + '\'' +
+                    ", employer='" + employer + '\'' +
+                    ", email='" + email + '\'' +
+                    ", city='" + city + '\'' +
+                    ", state='" + state + '\'' +
+                    '}';
+        }
     }
 
     class User {
@@ -79,6 +209,50 @@ public class GulimallSearchApplicationTests {
         // 提取有用的响应数据
         System.out.println(index);
 
+    }
+
+    @Test
+    public void searchData() throws IOException {
+        // 1.创建检索请求
+        SearchRequest searchRequest = new SearchRequest();
+        // 指定索引
+        searchRequest.indices("bank");
+        // 指定检索条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+//        sourceBuilder.from();
+//        sourceBuilder.size();
+        // 聚合分析
+        TermsAggregationBuilder ageAgg = AggregationBuilders.terms("ageAgg").field("age").size(10);
+        sourceBuilder.aggregation(ageAgg);
+        AvgAggregationBuilder balanceAgg = AggregationBuilders.avg("balanceAgg").field("balance");
+        sourceBuilder.aggregation(balanceAgg);
+        searchRequest.source(sourceBuilder);
+        System.out.println("检索条件：" + sourceBuilder.toString());
+        // 2.执行检索
+        SearchResponse searchResponse = client.search(searchRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+        System.out.println(searchResponse);
+        //3.分析结果
+        SearchHit[] hits = searchResponse.getHits().getHits();
+        for (SearchHit hit : hits) {
+            String asString = hit.getSourceAsString();
+            Account account = JSON.parseObject(asString, Account.class);
+            System.out.println("account:" + account);
+        }
+
+        // 获取这次检索到的聚合分析信息
+        Aggregations aggregations = searchResponse.getAggregations();
+        Terms ageAgg1 = aggregations.get("ageAgg");
+        for (Terms.Bucket bucket : ageAgg1.getBuckets()) {
+            String keyAsString = bucket.getKeyAsString();
+            System.out.println("年龄：" + keyAsString);
+            long docCount = bucket.getDocCount();
+            System.out.println("个数：" + docCount);
+            System.out.println("------------------");
+        }
+        Avg balanceAgg1 = aggregations.get("balanceAgg");
+        double value = balanceAgg1.getValue();
+        System.out.println("平均薪资：" + value);
     }
 
 }
