33bef0230f9dac16d54041e5e3c8444faff08cf5
nikec00 <760144709@qq.com>
2021-11-29 10:58:37 +0800

    引入模板引擎

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/app/CategoryController.java
@@ -0,0 +1,85 @@
+package com.atguigu.gulimall.product.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+// import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atguigu.gulimall.product.entity.CategoryEntity;
+import com.atguigu.gulimall.product.service.CategoryService;
+import com.atguigu.common.utils.R;
+
+
+/**
+ * 商品三级分类
+ *
+ * @author nkc
+ * @email 760144709@qq.com
+ * @date 2021-11-02 23:08:19
+ */
+@RestController
+@RequestMapping("product/category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 查出所有分类以及子列表，以树形结构组装起来
+     */
+    @RequestMapping("/list/tree")
+    // @RequiresPermissions("product:category:list")
+    public R list() {
+        List<CategoryEntity> entityList = categoryService.listWithTree();
+        return R.ok().put("data", entityList);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{catId}")
+    // @RequiresPermissions("product:category:info")
+    public R info(@PathVariable("catId") Long catId) {
+        CategoryEntity category = categoryService.getById(catId);
+
+        return R.ok().put("data", category);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    // @RequiresPermissions("product:category:save")
+    public R save(@RequestBody CategoryEntity category) {
+        categoryService.save(category);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    // @RequiresPermissions("product:category:update")
+    public R update(@RequestBody CategoryEntity category) {
+        categoryService.updateCasecade(category);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    // @RequiresPermissions("product:category:delete")
+    public R delete(@RequestBody Long[] catIds) {
+        categoryService.removeMenusByIds(Arrays.asList(catIds));
+        return R.ok();
+    }
+
+}
