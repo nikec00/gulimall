53a8d3196e7a8f09672e573e56c36e55b1ee2eeb
nikec00 <760144709@qq.com>
2022-01-02 17:10:41 +0800

    将商品添加购物车(完结)

--- gulimall-cart/src/main/java/com/atguigu/gulimall/cart/service/impl/CartServiceImpl.java
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/service/impl/CartServiceImpl.java
@@ -1,6 +1,7 @@
 package com.atguigu.gulimall.cart.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import com.atguigu.common.utils.R;
 import com.atguigu.gulimall.cart.feign.ProductFeignService;
@@ -10,6 +11,7 @@ import com.atguigu.gulimall.cart.vo.CartItem;
 import com.atguigu.gulimall.cart.vo.SkuInfoVo;
 import com.atguigu.gulimall.cart.vo.UserInfoTo;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -42,36 +44,46 @@ public class CartServiceImpl implements CartService {
     @Override
     public CartItem addCartItem(Long skuId, Integer num) {
         BoundHashOperations<String, Object, Object> cartOps = getCartOps();
-        // 远程调用商品信息
-        CartItem cartItem = new CartItem();
-        CompletableFuture<Void> getSkuInfoTask = CompletableFuture.runAsync(() -> {
-            R info = productFeignService.info(skuId);
-            if (info.getCode() == 0) {
-                SkuInfoVo skuInfo = info.getData("skuInfo", new TypeReference<SkuInfoVo>() {
-                });
-                // 商品添加到购物车
-                cartItem.setCheck(true);
-                cartItem.setCount(num);
-                cartItem.setImage(skuInfo.getSkuDefaultImg());
-                cartItem.setTitle(skuInfo.getSkuTitle());
-                cartItem.setSkuId(skuId);
-                cartItem.setPrice(skuInfo.getPrice());
+        String res = cartOps.get(skuId.toString()).toString();
+        if (StringUtils.isBlank(res)) {
+            // 远程调用商品信息
+            CartItem cartItem = new CartItem();
+            CompletableFuture<Void> getSkuInfoTask = CompletableFuture.runAsync(() -> {
+                R info = productFeignService.info(skuId);
+                if (info.getCode() == 0) {
+                    SkuInfoVo skuInfo = info.getData("skuInfo", new TypeReference<SkuInfoVo>() {
+                    });
+                    // 商品添加到购物车
+                    cartItem.setCheck(true);
+                    cartItem.setCount(num);
+                    cartItem.setImage(skuInfo.getSkuDefaultImg());
+                    cartItem.setTitle(skuInfo.getSkuTitle());
+                    cartItem.setSkuId(skuId);
+                    cartItem.setPrice(skuInfo.getPrice());
+                }
+            }, executor);
+            CompletableFuture<Void> getSkuSaleAttrValues = CompletableFuture.runAsync(() -> {
+                // 远程查询sku组合信息
+                List<String> values = productFeignService.getSkuSaleAttrValues(skuId);
+                cartItem.setSkuAttr(values);
+            }, executor);
+            // 等待所有任务都完成
+            try {
+                CompletableFuture.allOf(getSkuInfoTask, getSkuSaleAttrValues).get();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }, executor);
-        CompletableFuture<Void> getSkuSaleAttrValues = CompletableFuture.runAsync(() -> {
-            // 远程查询sku组合信息
-            List<String> values = productFeignService.getSkuSaleAttrValues(skuId);
-            cartItem.setSkuAttr(values);
-        }, executor);
-        // 等待所有任务都完成
-        try {
-            CompletableFuture.allOf(getSkuInfoTask, getSkuSaleAttrValues).get();
-        } catch (Exception e) {
-            e.printStackTrace();
+            String json = JSON.toJSONString(cartItem);
+            cartOps.put(skuId.toString(), json);
+            return cartItem;
+        } else {
+            int count = 0;
+            CartItem cartItem = JSON.parseObject(res, CartItem.class);
+            count = num + cartItem.getCount();
+            cartItem.setCount(count);
+            cartOps.put(skuId.toString(), JSON.toJSONString(cartItem));
+            return cartItem;
         }
-        String json = JSON.toJSONString(cartItem);
-        cartOps.put(skuId.toString(), json);
-        return cartItem;
     }
 
     /***
