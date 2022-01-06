262ddb9e325a4676f1ce90e98586bb7e58fd8c2a
nikec00 <760144709@qq.com>
2022-01-06 22:36:12 +0800

    rabbitmq消费端消息确认

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/controller/RabbitController.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/controller/RabbitController.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.order.controller;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.gulimall.order.entity.OrderEntity;
 import com.atguigu.gulimall.order.entity.OrderReturnReasonEntity;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,18 +25,18 @@ public class RabbitController {
     private RabbitTemplate rabbitTemplate;
 
     @GetMapping("/sendMq")
-    public String sendMq(@RequestParam(value = "number",defaultValue = "10") Integer number) {
+    public String sendMq(@RequestParam(value = "number", defaultValue = "10") Integer number) {
         for (int i = 0; i < 10; i++) {
             if (number % 2 == 0) {
                 OrderReturnReasonEntity reasonEntity = new OrderReturnReasonEntity();
                 reasonEntity.setId(1L);
                 reasonEntity.setCreateTime(new Date());
                 reasonEntity.setName("哈哈--" + i);
-                rabbitTemplate.convertAndSend("hello-java-exchange", "hello.java", reasonEntity);
+                rabbitTemplate.convertAndSend("hello-java-exchange", "hello.java", reasonEntity, new CorrelationData(UUID.randomUUID().toString()));
             } else {
                 OrderEntity orderEntity = new OrderEntity();
                 orderEntity.setOrderSn(UUID.randomUUID().toString());
-                rabbitTemplate.convertAndSend("hello-java-exchange", "hello2.java", orderEntity);
+                rabbitTemplate.convertAndSend("hello-java-exchange", "hello.java", orderEntity, new CorrelationData(UUID.randomUUID().toString()));
             }
         }
         return "ok";
--- gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
@@ -2,11 +2,13 @@ package com.atguigu.gulimall.order.service.impl;
 
 import com.atguigu.gulimall.order.entity.OrderEntity;
 import com.atguigu.gulimall.order.entity.OrderReturnReasonEntity;
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -20,7 +22,7 @@ import com.atguigu.gulimall.order.dao.OrderItemDao;
 import com.atguigu.gulimall.order.entity.OrderItemEntity;
 import com.atguigu.gulimall.order.service.OrderItemService;
 
-//@RabbitListener(queues = {"hello-java-queue"})//类+方法上（监听哪些队列即可）
+@RabbitListener(queues = {"hello-java-queue"})//类+方法上（监听哪些队列即可）
 @Service("orderItemService")
 public class OrderItemServiceImpl extends ServiceImpl<OrderItemDao, OrderItemEntity> implements OrderItemService {
 
@@ -34,21 +36,36 @@ public class OrderItemServiceImpl extends ServiceImpl<OrderItemDao, OrderItemEnt
         return new PageUtils(page);
     }
 
-//    @RabbitHandler
-    public void receiveMessage(OrderReturnReasonEntity content) {
+    @RabbitHandler
+    public void receiveMessage(Message message, OrderReturnReasonEntity content, Channel channel) {
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
         try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (InterruptedException e) {
+            if (deliveryTag % 2 == 0) {
+                channel.basicAck(deliveryTag, false);
+                System.out.println("签收了货物：=》" + deliveryTag);
+            } else {
+                channel.basicNack(deliveryTag,false,true);
+                System.out.println("拒绝签收货物：=》" + deliveryTag);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println("OrderReturnReasonEntity接受到消息。。。内容：" + content + "==>类型：" + content.getClass());
     }
 
-//    @RabbitHandler//标注在方法上（重载区分不同的消息）
-    public void receiveMessage2(OrderEntity content) {
+    @RabbitHandler//标注在方法上（重载区分不同的消息）
+    public void receiveMessage2(Message message, OrderEntity content, Channel channel) {
+        System.out.println();
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        System.out.println(deliveryTag);
         try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (InterruptedException e) {
+            if (deliveryTag % 2 == 0) {
+                channel.basicAck(deliveryTag, false);
+                System.out.println("签收了货物：=》" + deliveryTag);
+            } else {
+                System.out.println("拒绝签收货物：=》" + deliveryTag);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println("OrderEntity接受到消息。。。内容：" + content + "==>类型：" + content.getClass());
