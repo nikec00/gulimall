d4457fe199ff7938093653bcea08c3af318b22fb
nikec00 <760144709@qq.com>
2022-01-07 23:42:56 +0800

    订单页面Vo

---
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/vo/OrderConfirmVo.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.order.vo;
+
+import lombok.Data;
+
+/**
+ * @Description： 订单确认页需要用的数据展示
+ * @Author: nkc
+ * @Date: 2022/1/7 23:42
+ */
+@Data
+public class OrderConfirmVo {
+
+}
